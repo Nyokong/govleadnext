0080646e8071651af23b40861d9f4d59698953a8
Callmekay <50559078+Nyokong@users.noreply.github.com>
2025-08-26 12:44:35 +0200

    working on the header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -2,11 +2,12 @@ import Signin from "./signin";
 
 export const Header = () => {
   return (
-    <div className="w-full h-[100px] flex items-center justify-center">
-      Header goes here.
-      <br />
-      Add a button here
-      <Signin />
+    <div className="w-full h-[100px] flex items-center justify-evenly px-[20px] bg-[#111]">
+      <div className="bg-amber-700 w-full">tab 1</div>
+      <div className="bg-blue-700 w-full">tab 2</div>
+      <div>
+        <Signin />
+      </div>
     </div>
   );
 };
--- components/header/signin.tsx
+++ components/header/signin.tsx
@@ -1,6 +1,8 @@
 import { signIn } from "@/auth";
 import { Button } from "../ui/button";
 
+import { FcGoogle } from "react-icons/fc";
+
 export default function SignIn() {
   return (
     <form
@@ -9,8 +11,12 @@ export default function SignIn() {
         await signIn("google");
       }}
     >
-      <Button className="pointer" type="submit">
-        Signin with Google
+      <Button
+        className="cursor-pointer h-2x p-2 m-2 rounded-none"
+        type="submit"
+      >
+        Google
+        <FcGoogle />
       </Button>
     </form>
   );
