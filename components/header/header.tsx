1b14e9a927263ef1910946d994576fd2210cb3e3
Callmekay <50559078+Nyokong@users.noreply.github.com>
2025-08-26 11:04:33 +0200

    create new branch

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -2,6 +2,8 @@ export const Header = () => {
   return (
     <div className="w-full h-[100px] flex items-center justify-center">
       Header goes here.
+      <br />
+      Add a button here
     </div>
   );
 };
