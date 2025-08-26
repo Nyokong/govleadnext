79b3c6a4d43bdbc580e7725cc4b0582f6d72998d
Callmekay <50559078+Nyokong@users.noreply.github.com>
2025-08-26 11:43:00 +0200

    Adding auth

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,9 +1,12 @@
+import Signin from "./signin";
+
 export const Header = () => {
   return (
     <div className="w-full h-[100px] flex items-center justify-center">
       Header goes here.
       <br />
       Add a button here
+      <Signin />
     </div>
   );
 };
