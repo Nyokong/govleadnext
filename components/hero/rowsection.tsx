99b960216b3ef349643f3eb068128224efa0036b
Callmekay <50559078+Nyokong@users.noreply.github.com>
2025-09-02 20:02:02 +0200

    fixed some UI

--- components/hero/rowsection.tsx
+++ components/hero/rowsection.tsx
@@ -5,8 +5,11 @@ import { DiGoogleAnalytics } from 'react-icons/di';
 
 export default function Rowpage() {
   return (
-    <div className="relative">
-      <div className="md:hidden w-[100%] bg-[#0d0d0f] h-[240px] my-4 flex items-center justify-center gap-4">
+    <div className="relative bg-[#0d0d0f] h-[280px] italic">
+      <div className="w-full font-semibold h-[70px] flex justify-center items-center">
+        <h1 className="text-white text-[30px]">what to Expect ?</h1>
+      </div>
+      <div className="md:hidden w-[100%]  h-[160px] my-4 flex items-center justify-center gap-4">
         <div className="relative h-[100px] w-[100px] flex flex-col justify-center items-center gap-2 ">
           <span className="bg-orange-400 h-[90px] w-[90px] rounded-full flex justify-center items-center">
             <HiAcademicCap size={40} fill="white" />
